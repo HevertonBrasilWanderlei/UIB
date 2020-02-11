4c60e4994a3c3eb910f70bc6f07a445822d4ef44
Heverton Brasil Wanderlei <1192304923@acd.grupotiradentes.com>
2020-02-10 21:46:12 -0300

    Exercício banco 10 02 2020

---
+++ src/ClienteTeste.java
@@ -0,0 +1,20 @@
+
+public class ClienteTeste {
+
+	public static void main(String[] args) {
+		
+		Cliente clienteUm = new Cliente();
+		
+		clienteUm.nome = "It";
+	    clienteUm.cpf = "000.000.000-00";
+	    
+	    
+	    
+	    System.out.println("Nome:" + clienteUm.nome);
+	    
+	    
+				
+
+	}
+
+}
---
+++ src/Conta.java
@@ -0,0 +1,9 @@
+
+public class Conta {
+
+	Cliente clienteUm = new Cliente();
+	
+	conta = "";
+    clienteUm.cpf = "011.464.631-13";
+    
+}
